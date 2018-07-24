a864bb82482a5d436a46f7c4e34283f4b005c8e1
Divam <dfordivam@gmail.com>
2018-07-24 22:20:14 +0900

    Test communication over special device

--- test/Test.hs
+++ test/Test.hs
@@ -3,6 +3,25 @@
 module Main where
 
 import qualified Data.ByteString as BS
+import System.IO (openBinaryFile, IOMode(..))
+import Control.Concurrent
 
 main = do
   putStrLn "Hello from jsaddle-wasm-test"
+
+  out <- openBinaryFile "/dev/jsaddle_out" WriteMode
+  -- in <- openBinaryFile "/dev/jsaddle_in" ReadMode
+
+  BS.hPut out "Hello jsaddle"
+  BS.hPut out "More data to jsaddle"
+
+  forkIO $ do
+    BS.hPut out "Data from forked thread"
+    threadDelay 1000
+    BS.hPut out "Data from forked thread2"
+
+  BS.hPut out "More data from Main thread"
+  threadDelay 1000
+  BS.hPut out "More data from Main thread2"
+
+  putStrLn "Done"
