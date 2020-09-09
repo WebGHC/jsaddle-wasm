731ee059d8511da39a2e2f63c8662b67038ad938
Divam <dfordivam@gmail.com>
2020-09-09 21:51:54 +0900

    Support core-2 branch

--- src/Language/Javascript/JSaddle/Wasm.hs
+++ src/Language/Javascript/JSaddle/Wasm.hs
@@ -8,11 +8,10 @@
 
 
 module Language.Javascript.JSaddle.Wasm (
-  runWorker
-  , run
+  run
   ) where
 
-import Control.Monad (when, void, forever)
+import Control.Monad
 import Control.Concurrent (killThread, forkIO, threadDelay, yield)
 import Control.Concurrent.MVar
 import Control.Exception (try, AsyncException, IOException, throwIO, fromException, finally)
@@ -35,65 +34,8 @@ import Foreign.Storable
 
 import System.IO (openBinaryFile, IOMode(..))
 
-import Language.Javascript.JSaddle.Types (JSM, Batch, Results)
-import Language.Javascript.JSaddle.Run (syncPoint, runJavaScript)
-
-runWorker :: Int -> JSM () -> IO ()
-runWorker _ entryPoint = do
-  putStrLn "Starting JSaddle-Wasm"
-
-  jsInOut <- openBinaryFile "/dev/jsaddle_inout" ReadWriteMode
-
-  let
-    sendBatch :: Batch -> IO ()
-    sendBatch b = do
-      let payload = encode b
-          msg = (Binary.encode (fromIntegral $ BS.length payload :: Word32)) <> payload
-      BS.hPut jsInOut msg
-
-    receiveDataMessage :: IO (ByteString)
-    receiveDataMessage = loop
-      where
-        loop = do
-          threadDelay 1
-          try (BSIO.hGetNonBlocking jsInOut 4)
-            >>= \case
-              (Left (ex :: IOException)) -> loop
-              (Right v)
-                | BSIO.null v -> loop
-                | otherwise -> do
-                  -- Somehow we get this size in reverse!!
-                  let size = Binary.decode (BS.reverse $ BS.fromStrict v) :: Word32
-                  BS.fromStrict <$> BSIO.hGetNonBlocking jsInOut (fromIntegral size)
-
-    -- When to exit? never?
-    waitTillClosed = forever $ do
-      threadDelay (1*1000*1000)
-      putStrLn "JSaddle-Wasm heartbeat"
-      waitTillClosed
-
-  (processResult, _, start) <-
-    runJavaScript sendBatch entryPoint
-  forkIO . forever $ do
-    msgs <- receiveDataMessage
-    processIncomingMsgs processResult msgs
-
-  start
-  waitTillClosed
-
-processIncomingMsgs :: (Results -> IO ()) -> ByteString -> IO ()
-processIncomingMsgs cont msgs = if (BS.length msgs < 5)
-  then error $ "processIncomingMsgs: no more data while looping: " <> show msgs
-  else do
-    let
-      size = Binary.decode (BS.take 4 msgs) :: Word32
-      (thisMsg, rest) = BS.splitAt (fromIntegral $ 4 + size) msgs
-    case decode (BS.drop 4 thisMsg) of
-      Nothing -> error $ "jsaddle Results decode failed : " <> show thisMsg
-      Just r  -> cont r
-    case BS.length rest of
-      0 -> return ()
-      _ -> processIncomingMsgs cont rest
+import Language.Javascript.JSaddle.Types
+import Language.Javascript.JSaddle.Run (runJavaScript)
 
 -- JSaddle wasm HS side interface for running on main thread
 
@@ -106,9 +48,9 @@ run _ entryPoint = cJsaddleWasmInit =<< jsaddleInit entryPoint
 
 -- This contains everything needed to do a re-entry from foriegn export API
 data HsEnv = HsEnv
-  { _hsEnv_outgoingMessages :: MVar [Batch]
-  , _hsEnv_processResult :: (Results -> IO ())
-  , _hsEnv_processSyncResult :: (Results -> IO (Batch))
+  { _hsEnv_outgoingMessages :: MVar [TryReq]
+  , _hsEnv_processRsp :: ([Rsp] -> IO ())
+  , _hsEnv_processSyncCommand :: (SyncCommand -> IO [(Int, SyncBlockReq)])
   , _hsEnv_msgBufferPtr :: Ptr SharedMsgBuffer
   }
 
@@ -137,17 +79,17 @@ jsaddleInit entryPoint = do
   lockInit <- newEmptyMVar
 
   let
-    sendBatch :: Batch -> IO ()
+    sendBatch :: [TryReq] -> IO ()
     sendBatch b = do
-      modifyMVar_ outgoingMessages $ \a -> pure (a ++ [b])
+      modifyMVar_ outgoingMessages $ \a -> pure (a ++ b)
       tryPutMVar lockInit ()
       pure ()
 
-  (processResult, processSyncResult, start) <-
-    runJavaScript sendBatch entryPoint
+  (processRsp, processSyncCommand, env) <- runJavaScript sendBatch
+
   putStrLn "runJavaScript done"
 
-  forkIO $ start
+  forkIO $ runJSM entryPoint env
 
   -- This lock ensures that we have started the HS side threads
   -- TODO: Is this really needed?
@@ -164,42 +106,31 @@ jsaddleInit entryPoint = do
     poke ptr $ SharedMsgBuffer buf initSize
     pure ptr
 
-  newStablePtr $ HsEnv outgoingMessages processResult processSyncResult bufPtr
+  newStablePtr $ HsEnv outgoingMessages processRsp processSyncCommand bufPtr
 
 -- hsJsaddleProcessResult reads data from dataPtr, and writes back to the same place
 hsJsaddleProcessResult :: StablePtr HsEnv -> Bool -> Int -> IO Int64
 hsJsaddleProcessResult envPtr isSync dataLen = do
   -- putStrLn "Doing hsJsaddleProcessResult"
-  HsEnv outgoingMessages processResult processSyncResult bufPtr <- deRefStablePtr envPtr
+  HsEnv outgoingMessages processRsp processSyncCommand bufPtr <- deRefStablePtr envPtr
   SharedMsgBuffer dataPtr bufSize <- peek bufPtr
 
-  mResult <- if (dataLen == 0)
+  mBs <- if (dataLen == 0)
     then pure Nothing
-    else do
-      bs <- BS8.packCStringLen (dataPtr, dataLen)
+    else Just <$> BS8.packCStringLen (dataPtr, dataLen)
+
+  mReqs <- forM mBs $ \bs -> if isSync
+    then do
       case decode (BS.fromStrict bs) of
-        Nothing -> error $ "jsaddle Results decode failed : "
-        Just !r  -> pure $ Just r
+        Nothing -> error "Decode error: SyncCommand"
+        Just command -> Just <$> processSyncCommand command
+    else do
+      case decode (BS.fromStrict bs) of
+        Nothing -> error "Decode error: [Rsp]"
+        Just rsps -> Nothing <$ processRsp rsps
 
-  batches <- case (isSync, mResult) of
-    (True, Nothing) -> error "processSyncResult need single result"
-    (True, Just (r:[])) -> (:[]) <$> processSyncResult r
-    (True, Just (r:_)) -> error "processSyncResult got multiple results"
-    _ -> do
-      traverse (traverse processResult) mResult
-      -- Give the forked threads some time to work
-      let
-        loop m1 n = do
-          threadDelay (1000*1)
-          m2 <- modifyMVar outgoingMessages $ \m -> pure ([], m)
-          if (null m2) || (n < 0)
-            then pure m1
-            else loop (m1 ++ m2) (n - 1)
-      loop [] 10
-
-  case batches of
-    [] -> pure 0
-    m -> do
+  let
+    sendData m = do
       let outData = encode m
           dataLen = fromIntegral $ BS.length outData
       targetPtr <- if (dataLen > bufSize)
@@ -212,6 +143,22 @@ hsJsaddleProcessResult envPtr isSync dataLen = do
       -- putStrLn $ "outmsgsize: " <> show dataLen
       BSIO.useAsCStringLen (BS.toStrict outData) $ \(ptr, len) -> copyBytes targetPtr ptr len
       pure $ BS.length outData
+
+  case join mReqs of
+    Just reqs -> sendData reqs
+    Nothing -> do
+      -- Give the forked threads some time to work
+      let
+        loop m1 n = do
+          threadDelay (1000*1)
+          m2 <- modifyMVar outgoingMessages $ \m -> pure ([], m)
+          if (null m2) || (n < 0)
+            then pure m1
+            else loop (m1 ++ m2) (n - 1)
+      reqs <- loop [] 10
+      case reqs of
+        [] -> pure 0
+        _ -> sendData reqs
 
 -- The Char* and size values are read by JS code
 -- So this should be in sync with JS
